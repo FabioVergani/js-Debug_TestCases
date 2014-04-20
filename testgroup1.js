a5c68c02b7ae7516d8c136f0d438e1ba5ddcb1e3
FabioVergani <fabio.vergani.1975@gmail.com>
2014-04-20 18:59:20 +0200

    Update testgroup1.js

--- testgroup1.js
+++ testgroup1.js
@@ -13,17 +13,32 @@ EmptyObject ={},SimpleObject ={a: 1,b: 2},ComplexObject={a:{b: 1}};
 
 //-
 
-Test('#Undefined',[undefined,void(0),indefinito]);
+Test('#Undefined',[undefined,void(0),indefinito]);/*all: undefined*/
 
 Test('#Strings',[
-	""," ","  ","\t","\n","\r\n","bla",
+	"",String(""),String(EmptyArray),
+	" ","  ","\t","\n","\r\n","bla",
 	String(EmptyObject),String(SimpleObject),
-	String(EmptyArray),String(SimpleArray),
-	String(""),String("abc"),
+	String(SimpleArray),
+	String("abc"),
 	String(false),String(true),
 	String(0),String(1),
-	String(undefined),String(indefinito),String(Object),String(String)
+	String(undefined),String(indefinito),
+	String(Object),String(String)
 ]);
+/*
+""  "" ""
+" " "  " "	" "\n" "\r\n" "bla"
+"[object Object]" "[object Object]"
+"1,2,3"
+"abc"
+"false" "true"
+"0" "1"
+"undefined" "undefined"
+"function Object() { [native code] }"
+"function String() { [native code] }"
+*/
+
 Test('#Strings-Obj',[
 	new String(),
 	new String(""),new String("abc"),
@@ -33,21 +48,44 @@ Test('#Strings-Obj',[
 	new String(EmptyObject),new String(SimpleObject),
 	new String(undefined),new String(indefinito),new String(Object),new String(String)
 ]);
+/*
+all: String
+*/
 
 Test('#Numbers',[
 	0,1,666.0,3.14,
 	Math.PI,Math.SQRT2,Math.SQRT1_2,Math.E,Math.LN2,Math.LN10,Math.LOG2E,Math.LOG10E,
-	Number(),Number(""),
-	Number(false),Number(true),
-	Number(0),Number(1)
+	Number(),Number(""),Number(false),Number(0),
+	Number(true),Number(1)
 ]);
+/*
+0,1,666,3.14
+3.141592653589793
+1.4142135623730951
+0.7071067811865476
+2.718281828459045
+0.6931471805599453
+2.302585092994046
+1.4426950408889634
+0.4342944819032518
+0,0,0,0
+1,1
+*/
+
 Test('#SpecialNumbers',[
+	Number(EmptyArray),
 	Infinity,NaN,
 	Number("abc"),
-	Number(EmptyArray),Number(SimpleArray),
+	Number(SimpleArray),
 	Number(EmptyObject),Number(SimpleObject),
 	Number(undefined),Number(indefinito),Number(Object),Number(Number)
 ]);
+/*
+0
+Infinity
+other:NaN
+*/
+
 Test('#Numbers-Obj',[
 	new Number(),
 	new Number(""),new Number("abc"),
@@ -57,6 +95,9 @@ Test('#Numbers-Obj',[
 	new Number(EmptyObject),new Number(SimpleObject),
 	new Number(undefined),new Number(indefinito),new Number(Object),new Number(Number)
 ]);
+/*
+all: Number
+*/
 
 Test('#Booleans',[
 	true,Boolean("abc"),Boolean(true),Boolean(1),Boolean(Object),Boolean(Boolean),
@@ -64,6 +105,11 @@ Test('#Booleans',[
 	Boolean(EmptyObject),Boolean(SimpleObject),
 	false,Boolean(),Boolean(""),Boolean(false),Boolean(0),Boolean(undefined),Boolean(indefinito)
 ]);
+/*
+true true true true true true true true true true
+false false false false false false false
+*/
+
 Test('#Booleans-Obj',[
 	new Boolean(),
 	new Boolean(""),new Boolean("abc"),
@@ -73,13 +119,55 @@ Test('#Booleans-Obj',[
 	new Boolean(EmptyObject),new Boolean(SimpleObject),
 	new Boolean(undefined),new Boolean(indefinito),new Boolean(Object),new Boolean(Boolean)
 ]);
+/*
+all: Boolean
+*/
 
-Test('#Functions',[function(){},function pippo(){},Function("a","b","return a + b"),Function(""),Function()]);
+Test('#Functions',[
+function(){},function pippo(){},
+Function("a","b","return a + b"),
+Function(""),
+Function()
+]);
+/*
+function (){}
+function pippo(){}
+function anonymous(a,b
+function anonymous() {
+function anonymous() {
+*/
+
 Test('#FunctionsNative ',[Math.sin,isNaN,Date,Object,Function,]);
-Test('#Functions-Obj',[new Function("a","b","return a + b"),new Function(""),new Function()]);
+/*
+function sin() { [native code] }
+function isNaN() { [native code] }
+function Date() { [native code] }
+function Object() { [native code] }
+function Function() { [native code] }
+*/
+
+Test('#Functions-Obj',[
+new Function("a","b","return a + b"),
+new Function(""),
+new Function()
+]);
+/*
+function anonymous(a,b
+function anonymous() {
+function anonymous() {
+*/
 
 Test('#Rgxs',[/(zzz)/,/(\b)/gi]);
+/*
+/(zzz)/
+/(\b)/gi
+*/
+
 Test('#Rgxs-Obj',[new RegExp(),new RegExp("(\w)","i")]);
+/*
+/(?:)/
+/(w)/i
+*/
 
 Test('#Dates',[
 	Date(),Date(""),
@@ -88,13 +176,28 @@ Test('#Dates',[
 	Date(false),Date(0),
 	Date(true),Date(1)
 ]);
+/*
+All2String: "Sun Apr 20 2014 18:18:20 GMT+0200 (W. Europe Daylight Time)"
+*/
+
 Test('#Dates-Obj',[
-	new Date(),new Date(""),
-	new Date("December 17,1995 03:24:00"),new Date("1995-12-17T03:24:00"),
-	new Date(1995,11,17),new Date(1995,11,17,3,24,0),new Date(98,1),
-	new Date(false),new Date(0),
-	new Date(true),new Date(1)
+	new Date(""),
+	new Date(),
+	new Date("1995-12-17T03:24:00"),
+	new Date(1995,11,17),
+	new Date("December 17,1995 03:24:00"),new Date(1995,11,17,3,24,0),
+	new Date(false),new Date(0),new Date(true),new Date(1),
+	new Date(98,1)
 ]);
+/*
+0: Invalid Date
+1: Sun Apr 20 2014 18:18:20 GMT+0200 (W. Europe Daylight Time)
+2: Sun Dec 17 1995 04:24:00 GMT+0100 (W. Europe Standard Time)
+3: Sun Dec 17 1995 00:00:00 GMT+0100 (W. Europe Standard Time)
+4-5: Sun Dec 17 1995 03:24:00 GMT+0100 (W. Europe Standard Time)
+6-9: Thu Jan 01 1970 01:00:00 GMT+0100 (W. Europe Standard Time)
+10: Sun Feb 01 1998 00:00:00 GMT+0100 (W. Europe Standard Time)
+*/
 
 Test('#Errors',[
 	new Error("Error-message"),
@@ -105,20 +208,47 @@ Test('#Errors',[
 	new RangeError("Range Error-message"),
 	new EvalError("Eval Error-message")
 ]);
+/*
+0: Error
+1: URIError
+2: TypeError
+3: SyntaxError
+4: ReferenceError
+5: RangeError
+6: EvalError
+*/
 
 Test('#Objects',[null,EmptyObject,SimpleObject,ComplexObject,Object()]);
+/*
+0: null
+1-4: Object
+*/
+
 Test('#Objects-Obj',[
 	new Object(),new Object(undefined),new Object(EmptyObject),new Object(SimpleObject),
 	new Object(""),
 	new Object(0),new Object(1),
 	new Object(false),new Object(true)
 ]);
+/*
+0-3: Object
+4: String
+5-6: Number
+7-8: Boolean
+*/
 
 Test('#Array',[EmptyArray,SimpleArray,ComplexArray]);
+/*
+0: Array[0]
+1: Array[3]
+2: Array[2]
+*/
+
 Test('#Array-Obj',[
-	new Array(),new Array(undefined),new Array(""),new Array(1,2,3),
-	new Array(EmptyArray),
-	new Array(SimpleArray),
-	new Array(0),new Array(false),
-	new Array(1),new Array(true)
+	new Array(),new Array(0),//Array[0]
+	new Array(1,2,3),//Array[3]
+	new Array(""),new Array(undefined),new Array(false),new Array(1),new Array(true),//Array[1]
+	new Array(EmptyArray),new Array(SimpleArray)//Array[1]
 ]);
+/*
+*/
